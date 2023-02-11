de40be5729b16e6101ec7af9581d59efeab17cfe
omarait-mlouk <72560701+omarait-mlouk@users.noreply.github.com>
2023-02-11 16:24:18 +0100

    add the final feature real time rates

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ load_dotenv()
 # Get the API key from the environment variables
 api_key = os.getenv("API_KEY")
 
-def make_get_request(url, headers=None, timeout=5):
+def make_get_request(endpoint, headers=None, timeout=5):
     """
-    Make a GET request to the specified URL using the given headers and timeout.
+    Make a GET request to the specified ENDPOINT using the given headers and timeout.
 
     Parameters:
-        url (str): The URL to make the request to.
+        endpoint (str): The ENDPOINT to make the request to.
         headers (dict, optional): A dictionary of headers to include in the request.
         timeout (int, optional): The timeout value for the request in seconds.
 
@@ -27,7 +27,7 @@ def make_get_request(url, headers=None, timeout=5):
     """
     try:
         # Make the GET request
-        response = requests.get(url, headers=headers, timeout=timeout)
+        response = requests.get(endpoint, headers=headers, timeout=timeout)
         # Raise an exception if the response has a status code indicating an error
         response.raise_for_status()
     except requests.exceptions.RequestException as error:
@@ -43,11 +43,11 @@ def get_currencies():
     """
     # Set the headers to include the API key
     headers = {'apikey': api_key}
-    # Build the URL to get the available currencies
-    url = f"https://api.apilayer.com/fixer/symbols"
+    # Build the ENDPOINT to get the available currencies
+    endpoint = f"https://api.apilayer.com/fixer/symbols"
     try:
         # Get the response content from the API
-        response_content = make_get_request(url, headers, timeout=5)
+        response_content = make_get_request(endpoint, headers, timeout=5)
         # Load the JSON data
         parsed_data = json.loads(response_content)
         # Check if the success key is true
@@ -76,12 +76,12 @@ def convert_currency(from_currency, to_currency, amount):
     """
     # Set the headers to include the API key
     headers = {'apikey': api_key}
-    # Build the URL to convert the currency
-    url = f"https://api.apilayer.com/fixer/convert?to={to_currency}"\
+    # Build the ENDPOINT to convert the currency
+    endpoint = f"https://api.apilayer.com/fixer/convert?to={to_currency}"\
     f"&from={from_currency}&amount={amount}"
     try:
         # Get the response content from the API
-        response_content = make_get_request(url, headers, timeout=10)
+        response_content = make_get_request(endpoint, headers, timeout=10)
         parsed_data = json.loads(response_content)
         # Check if the success key is true
         if parsed_data.get("success", False):
@@ -93,6 +93,58 @@ def convert_currency(from_currency, to_currency, amount):
             # If success is not true, handle the error
             error_message = parsed_data.get("message", "An unknown error occurred")
             print(f"Error while accessing symbols: {error_message}")
+    except Exception as error:
+        # Print an error message if the request failed
+        print(f'Request failed with error: {error}')
+
+
+def get_exchange_rate(base=None, symbols=None):
+    """
+    Retrieve the real-time exchange rate between two currencies. If either `from_currency` or `to_currency` are not specified, 
+    the function returns the exchange rate of the default currency (e.g., USD) to all other currencies.
+
+    Parameters:
+        from_currency (str, optional): The code of the currency to convert from.
+        to_currency (str, optional): The code of the currency to convert to.
+    """
+    # Set the headers to include the API key
+    headers = {'apikey': api_key}
+    # Define the API endpoint
+    endpoint = "https://api.apilayer.com/fixer/latest"
+    params = ['?']
+    if symbols:
+        # If symbols is provided, add it to the params list
+        params.append(f"symbols={symbols}")
+    # Check if the base parameter is provided
+    if base:
+        # If base is provided, add it to the params list
+        params.append(f"base={base}")
+    # Check if there are any parameters in the params list
+    if params:
+        # Join the parameters with an "&" and add them to the endpoint
+        endpoint += "&".join(params)
+    # Make the GET request to the API endpoint
+    try:
+        # Get the response content from the API
+        response_content = make_get_request(endpoint, headers, timeout=5)
+        # Load the JSON data
+        parsed_data = json.loads(response_content)
+        # Check if the success key is true
+        if parsed_data.get("success", False):
+            # Access the base
+            base = parsed_data["base"]
+            # Access the date
+            date = parsed_data["date"]
+            # Access the rates
+            rates = parsed_data["rates"]
+            print(f"\nBase: {base}\tDate: {date}")
+            print("Rates: ")
+            # Iterate over the rates and print the currency code and its rate
+            for currency_code, rate in rates.items():
+                print(f"{currency_code}: {rate}")
+        else:
+            # If success is not true, handle the error
+            print("Error while accessing data")
     except Exception as error:
         # Print an error message if the request failed
         print(f'Request failed with error: {error}')
@@ -111,7 +163,8 @@ def quit_program():
 choice_handlers = {
     1: get_currencies,
     2: convert_currency,
-    3: quit_program,
+    3: get_exchange_rate,
+    4: quit_program,
 }
 
 def handle_choices(choice):
@@ -137,7 +190,12 @@ def handle_choices(choice):
                 from_currency = input("Enter the from currency: ")
                 to_currency = input("Enter the to currency: ")
                 amount = input("Enter the amount: ")
-                return handler(from_currency, to_currency, amount)
+                handler(from_currency, to_currency, amount)
+            elif int_value == 3:
+                # If the choice is 2 (convert currency), retrieve the from and to currency, and the amount
+                base = input("Enter the three-letter currency code of your preferred base currency: ")
+                symbols = input("Enter a list of comma-separated currency codes to limit output currencies: ")
+                handler(base, symbols)
             else:
                 # If the choice is 1 (get currencies) or 3 (quit), call the handler function
                 handler()
@@ -154,11 +212,12 @@ def main():
     while True:
         # Ask the user to make a choice
         choice = int(input(
-            "\n########### MENU ##############\n"
-            "## 1 - Available currencies. ##\n"
-            "## 2 - Convert a currency.   ##\n"
-            "## 3 - Quit.                 ##\n"
-            "###############################\n"
+            "\n########### MENU #################\n"
+            "## 1 - Available currencies.    ##\n"
+            "## 2 - Convert a currency.      ##\n"
+            "## 3 - Real-time exchange rate. ##\n"
+            "## 4 - Quit.                    ##\n"
+            "##################################\n"
             "Choose in the menu: "
         ))
         # Handle the user's choice
